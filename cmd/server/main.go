2a09b7cfae3b83a43ba662c7fada631bb3adebde
yangshujie <yangshujie@fangcunyisheng.com>
2025-02-21 18:21:08 +0800

    pref refactor main func

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/yshujie/blog-serve/internal/config"
+	"github.com/yshujie/blog-serve/pkg/app"
+)
+
+func main() {
+	// 加载配置文件
+	cfg, err := config.LoadConfig("config.yaml")
+	if err != nil {
+		log.Fatalf("Failed to load config: %v", err)
+	}
+
+	// 初始化 app
+	app := app.NewApp(cfg)
+
+	// 启动 app
+	app.Run()
 }
