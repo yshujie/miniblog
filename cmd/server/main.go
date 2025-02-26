0ccd977d9a6cc33d307fc1b628205b584ebb906b
yangshujie <yangshujie@fangcunyisheng.com>
2025-02-26 16:14:25 +0800

    pref add runnung log

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,23 +1,27 @@
 package main
 
 import (
-	"fmt"
-	"log"
-
 	"github.com/yshujie/blog-serve/internal/config"
 	"github.com/yshujie/blog-serve/pkg/app"
+	"github.com/yshujie/blog-serve/pkg/log"
 )
 
 func main() {
+	// 初始化日志
+	logger := log.NewLogger()
+	logger.Info("Starting server...")
+
 	// 加载配置文件
+	logger.Info("Loading config...")
 	cfg, err := config.LoadConfig("internal/config/config.yaml")
 	if err != nil {
-		log.Fatalf("Failed to load config: %v", err)
+		logger.Error("Failed to load config: %v", err)
+		return
 	}
-
-	// 初始化 app
-	app := app.NewApp(cfg)
+	logger.Info("Config loaded successfully")
 
 	// 启动 app
-	app.Run()
+	logger.Info("Starting app...")
+	app.NewApp(cfg).Run()
+	logger.Info("App started successfully")
 }
--- pkg/log/log.go
+++ pkg/log/log.go
@@ -1,5 +1,7 @@
 package log
 
+import "fmt"
+
 type Logger struct{}
 
 func NewLogger() *Logger {
@@ -8,4 +10,34 @@ func NewLogger() *Logger {
 
 func (l *Logger) Info(format string, args ...interface{}) {
 	// 实现日志记录逻辑
+	l.writeLog("info", format, args...)
+}
+
+func (l *Logger) Error(format string, args ...interface{}) {
+	// 实现日志记录逻辑
+	l.writeLog("error", format, args...)
+}
+
+func (l *Logger) Fatal(format string, args ...interface{}) {
+	// 实现日志记录逻辑
+	l.writeLog("fatal", format, args...)
+}
+
+func (l *Logger) Debug(format string, args ...interface{}) {
+	// 实现日志记录逻辑
+	l.writeLog("debug", format, args...)
+}
+
+func (l *Logger) Warn(format string, args ...interface{}) {
+	// 实现日志记录逻辑
+	l.writeLog("warn", format, args...)
+}
+
+func (l *Logger) writeLog(level string, format string, args ...interface{}) {
+	if args == nil {
+		fmt.Println("[", level, "]", format)
+	} else {
+		fmt.Println("[", level, "]", format, args)
+	}
+
 }
