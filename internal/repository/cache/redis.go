16d443db5f99bbba1a41eb4c92afe93792c52133
yangshujie <yangshujie@fangcunyisheng.com>
2025-02-26 16:19:06 +0800

    pref add log

--- internal/repository/cache/redis.go
+++ internal/repository/cache/redis.go
@@ -7,16 +7,28 @@ import (
 
 	"github.com/redis/go-redis/v9"
 	"github.com/yshujie/blog-serve/internal/config"
+	"github.com/yshujie/blog-serve/pkg/log"
 )
 
 var rdb *redis.Client
 
 func Init(cfg *config.Redis) {
+	logger := log.NewLogger()
+	logger.Info("Connecting to Redis...")
+
 	rdb = redis.NewClient(&redis.Options{
 		Addr:     fmt.Sprintf("%s:%d", cfg.Host, cfg.Port),
 		Password: cfg.Password,
 		DB:       cfg.DB,
 	})
+	// 测试连接
+	_, err := rdb.Ping(context.Background()).Result()
+	if err != nil {
+		logger.Error("Failed to connect to Redis: %v", err)
+		panic(err)
+	}
+
+	logger.Info("Redis connected successfully")
 }
 
 func Close() {
