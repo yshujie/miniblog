9602c17ee3c4faf836743b1b68ee4892d8d7b1eb
yangshujie <yangshujie@fangcunyisheng.com>
2025-03-07 16:04:51 +0800

    pref add alias to model package

--- internal/repository/mysql/article.go
+++ internal/repository/mysql/article.go
@@ -2,7 +2,7 @@ package mysql
 
 import (
 	"github.com/jinzhu/gorm"
-	"github.com/yshujie/blog-serve/internal/model"
+	m "github.com/yshujie/blog-serve/internal/model"
 )
 
 type ArticleRepository struct {
@@ -15,34 +15,34 @@ func NewArticleRepository(db *gorm.DB) *ArticleRepository {
 }
 
 // create article
-func (r *ArticleRepository) Create(article *model.Article) error {
+func (r *ArticleRepository) Create(article *m.Article) error {
 	return r.db.Create(article).Error
 }
 
 // update article
-func (r *ArticleRepository) Update(article *model.Article) error {
+func (r *ArticleRepository) Update(article *m.Article) error {
 	return r.db.Save(article).Error
 }
 
 // delete article
-func (r *ArticleRepository) Delete(article *model.Article) error {
+func (r *ArticleRepository) Delete(article *m.Article) error {
 	return r.db.Delete(article).Error
 }
 
 // get article by id
-func (r *ArticleRepository) GetById(id int) (*model.Article, error) {
-	var article model.Article
+func (r *ArticleRepository) GetById(id int) (*m.Article, error) {
+	var article m.Article
 	return &article, r.db.Where("id = ?", id).First(&article).Error
 }
 
 // get article by title
-func (r *ArticleRepository) GetByTitle(title string) (*model.Article, error) {
-	var article model.Article
+func (r *ArticleRepository) GetByTitle(title string) (*m.Article, error) {
+	var article m.Article
 	return &article, r.db.Where("title = ?", title).First(&article).Error
 }
 
 // get all articles
-func (r *ArticleRepository) GetAll() ([]model.Article, error) {
-	var articles []model.Article
+func (r *ArticleRepository) GetAll() ([]m.Article, error) {
+	var articles []m.Article
 	return articles, r.db.Find(&articles).Error
 }
