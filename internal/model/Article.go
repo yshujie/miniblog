4a0f2a21b4ef397fd1481bd4f70efb51eee40455
yangshujie <yangshujie@fangcunyisheng.com>
2025-03-07 14:59:49 +0800

    pref delete abandoned models

--- internal/model/Article.go
+++ internal/model/Article.go
@@ -2,11 +2,9 @@ package model
 
 // 文章 model
 type Article struct {
-	Title    string   `gorm:"type:varchar(100);not null" json:"title"`
-	Cid      int      `gorm:"type:int;not null" json:"cid"`
-	Desc     string   `gorm:"type:varchar(200)" json:"desc"`
-	Content  string   `gorm:"type:longtext;not null" json:"content"`
-	Img      string   `gorm:"type:varchar(200)" json:"img"`
-	Category Category `gorm:"foreignkey:Cid"`
-	Comments []Comment
+	Title   string `gorm:"type:varchar(100);not null" json:"title"`
+	Cid     int    `gorm:"type:int;not null" json:"cid"`
+	Desc    string `gorm:"type:varchar(200)" json:"desc"`
+	Content string `gorm:"type:longtext;not null" json:"content"`
+	Img     string `gorm:"type:varchar(200)" json:"img"`
 }
