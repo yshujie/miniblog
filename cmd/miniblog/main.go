117eb769fa8e4721e9890e352971398117c7649d
yangshujie <yangshujie@fangcunyisheng.com>
2025-04-22 16:48:18 +0800

    feat clear /cmd/miniblog/main.go

--- cmd/miniblog/main.go
+++ cmd/miniblog/main.go
@@ -1,17 +1,5 @@
 package main
 
-import (
-	"github.com/yshujie/miniblog/internal/config"
-	"github.com/yshujie/miniblog/pkg/app"
-)
-
 func main() {
-	// 加载配置文件
-	cfg, err := config.LoadConfig()
-	if err != nil {
-		return
-	}
 
-	// 启动 app
-	app.NewApp(cfg).Run()
 }
