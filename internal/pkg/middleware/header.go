88b82da615f0b32a755bd1f200cf55677b5a2f29
yangshujie <yangshujie@fangcunyisheng.com>
2025-05-06 19:26:14 +0800

    feat create cors middleware

---
+++ internal/pkg/middleware/header.go
@@ -0,0 +1,16 @@
+package middleware
+
+import "github.com/gin-gonic/gin"
+
+func Cors(ctx *gin.Context) {
+	if ctx.Request.Method != "OPTIONS" {
+		ctx.Next()
+	} else {
+		ctx.Header("Access-Control-Allow-Origin", "*")
+		ctx.Header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS")
+		ctx.Header("Access-Control-Allow-Headers", "authorization,origin,content-type,accept,x-requested-with")
+		ctx.Header("Allow", "HEAD,GET,POST,PUT,PATCH,DELETE,OPTIONS")
+		ctx.Header("Content-Type", "application/json")
+		ctx.AbortWithStatus(200)
+	}
+}
