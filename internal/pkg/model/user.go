c0d3c2bc8cec4f5ede809a6bd9e18b325c470a3d
yangshujie <yangshujie@fangcunyisheng.com>
2025-05-06 20:48:54 +0800

    feat 实现 User struct

--- internal/pkg/model/user.go
+++ internal/pkg/model/user.go
@@ -1,1 +1,19 @@
 package model
+
+import "time"
+
+type User struct {
+	ID        int64     `gorm:"column:id;primary_key"`
+	Username  string    `gorm:"column:username;not null"`
+	Password  string    `gorm:"column:password;not null"`
+	Nickname  string    `gorm:"column:nickname"`
+	Email     string    `gorm:"column:email"`
+	Phone     string    `gorm:"column:phone"`
+	CreatedAt time.Time `gorm:"column:createdAt"`
+	UpdatedAt time.Time `gorm:"column:updatedAt"`
+}
+
+// TableName 指定表名
+func (u *User) TableName() string {
+	return "user"
+}
