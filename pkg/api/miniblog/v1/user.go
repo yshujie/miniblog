5cfef3dc0c16e78743712c4e0a808553c1cef1be
yangshujie <yangshujie@fangcunyisheng.com>
2025-05-07 21:04:33 +0800

    feat 新增 Request DTO

--- pkg/api/miniblog/v1/user.go
+++ pkg/api/miniblog/v1/user.go
@@ -1,9 +1,27 @@
 package v1
 
+// CreateUserRequest 创建用户请求
 type CreateUserRequest struct {
 	Username string `json:"username" valid:"alphanum,required,stringlength(1|255)"`
 	Password string `json:"password" valid:"required,stringlength(6|18)"`
 	Nickname string `json:"nickname" valid:"required,stringlength(1|255)"`
 	Email    string `json:"email" valid:"required,email"`
 	Phone    string `json:"phone" valid:"required,stringlength(11|11)"`
+}
+
+// LoginRequest 登录请求
+type LoginRequest struct {
+	Username string `json:"username" valid:"required,stringlength(1|255)"`
+	Password string `json:"password" valid:"required,stringlength(6|18)"`
+}
+
+// LoginResponse 登录响应
+type LoginResponse struct {
+	Token string `json:"token"`
+}
+
+// ChangePasswordRequest 修改密码请求
+type ChangePasswordRequest struct {
+	OldPassword string `json:"oldPassword" valid:"required,stringlength(6|18)"`
+	NewPassword string `json:"newPassword" valid:"required,stringlength(6|18)"`
 }
