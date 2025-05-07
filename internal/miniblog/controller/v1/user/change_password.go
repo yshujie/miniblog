f804086bf6e1480a0f4d6a351afb776042bb77e4
yangshujie <yangshujie@fangcunyisheng.com>
2025-05-07 21:37:36 +0800

    feat 实现 chang'e'P    实现 changePassword

---
+++ internal/miniblog/controller/v1/user/change_password.go
@@ -0,0 +1,38 @@
+package user
+
+import (
+	"github.com/asaskevich/govalidator"
+	"github.com/gin-gonic/gin"
+	"github.com/yshujie/miniblog/internal/pkg/core"
+	"github.com/yshujie/miniblog/internal/pkg/errno"
+	"github.com/yshujie/miniblog/internal/pkg/log"
+	v1 "github.com/yshujie/miniblog/pkg/api/miniblog/v1"
+)
+
+func (ctrl *UserController) ChangePassword(c *gin.Context) {
+	log.C(c).Infow("ChangePassword function called")
+
+	// 从请求中获取修改密码请求参数
+	var r v1.ChangePasswordRequest
+	if err := c.ShouldBindJSON(&r); err != nil {
+		core.WriteResponse(c, errno.ErrBind, nil)
+		return
+	}
+
+	// 验证请求参数
+	if _, err := govalidator.ValidateStruct(r); err != nil {
+		core.WriteResponse(c, errno.ErrInvalidParameter.SetMessage(err.Error()), nil)
+
+		return
+	}
+
+	// 调用业务层修改密码
+	if err := ctrl.b.Users().ChangePassword(c, c.Param("name"), &r); err != nil {
+		core.WriteResponse(c, err, nil)
+
+		return
+	}
+
+	// 返回修改密码响应
+	core.WriteResponse(c, nil, nil)
+}
