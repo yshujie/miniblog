60766d35ebb0add02cd1e6001416a1a1fe7ecf8c
yangshujie <yangshujie@fangcunyisheng.com>
2025-05-07 17:55:06 +0800

    pref add CreateUserRequest struct

--- pkg/api/miniblog/v1/user.go
+++ pkg/api/miniblog/v1/user.go
@@ -1,1 +1,9 @@
 package v1
+
+type CreateUserRequest struct {
+	Username string `json:"username" valid:"alphanum,required,stringlength(1|255)"`
+	Password string `json:"password" valid:"required,stringlength(6|18)"`
+	Nickname string `json:"nickname" valid:"required,stringlength(1|255)"`
+	Email    string `json:"email" valid:"required,email"`
+	Phone    string `json:"phone" valid:"required,stringlength(11|11)"`
+}
