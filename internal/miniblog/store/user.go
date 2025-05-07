7e60ca0cf1c8ac61145783d4396aaa444989928a
yangshujie <yangshujie@fangcunyisheng.com>
2025-05-07 21:10:08 +0800

    feat use store 实现 获取\更新函数

--- internal/miniblog/store/user.go
+++ internal/miniblog/store/user.go
@@ -26,3 +26,18 @@ func newUsers(db *gorm.DB) *users {
 func (u *users) Create(ctx context.Context, user *model.UserM) error {
 	return u.db.Create(user).Error
 }
+
+// GetUser 获取用户
+func (u *users) Get(ctx context.Context, username string) (*model.UserM, error) {
+	var user model.UserM
+	if err := u.db.Where("username = ?", username).First(&user).Error; err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+// Update 更新用户
+func (u *users) Update(ctx context.Context, user *model.UserM) error {
+	return u.db.Save(user).Error
+}
