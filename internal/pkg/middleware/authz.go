79f37a9131f766c5d17b4c41982a350faab81999
yangshujie <yangshujie@fangcunyisheng.com>
2025-05-07 22:26:06 +0800

    feat 新增授权中间件

---
+++ internal/pkg/middleware/authz.go
@@ -0,0 +1,32 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/yshujie/miniblog/internal/pkg/core"
+	"github.com/yshujie/miniblog/internal/pkg/errno"
+	"github.com/yshujie/miniblog/internal/pkg/known"
+	"github.com/yshujie/miniblog/internal/pkg/log"
+)
+
+type Auther interface {
+	Authorize(sub, obj, act string) (bool, error)
+}
+
+// Authz 是 Gin 中间件，用来进行请求授权.
+func Authz(a Auther) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// 从上下文中获取用户名
+		sub := c.GetString(known.XUsernameKey)
+		// 获取请求的URL路径
+		obj := c.Request.URL.Path
+		// 获取请求的方法
+		act := c.Request.Method
+
+		log.Debugw("Build authorize context", "sub", sub, "obj", obj, "act", act)
+		if allowed, _ := a.Authorize(sub, obj, act); !allowed {
+			core.WriteResponse(c, errno.ErrUnauthorized, nil)
+			c.Abort()
+			return
+		}
+	}
+}
