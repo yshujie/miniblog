5cc9e682ac10a9c63a8015e0725932e32a7f2580
yangshujie <yangshujie@fangcunyisheng.com>
2025-05-13 13:01:23 +0800

    ci 更正数据库导入脚本

--- scripts/init_mysql_schem.sh
+++ scripts/init_mysql_schem.sh
@@ -9,14 +9,25 @@ if ! mysqladmin -u root -p ping; then
     exit 1
 fi
 
+# 如果 miniblog 数据库存在，则不再导入
+if mysql -u root -p -e "show databases;" | grep -q "miniblog"; then
+    echo "miniblog 数据库已存在，不再导入"
+    exit 0
+fi
+
 # 导入数据库
 mysql -u root -p miniblog < /data/mysql/data/miniblog.sql
 
 # 验证数据库，查看 miniblog 数据库是否创建成功
 echo "验证数据库，查看 miniblog 数据库是否创建成功"
-mysql -u root -p -e "show databases;"
+if ! mysql -u root -p -e "show databases;" | grep -q "miniblog"; then
+    echo "miniblog 数据库创建失败"
+    exit 1
+fi
 
 # 验证 miniblog 数据库是否导入成功
 echo "验证 miniblog 数据库是否导入成功"
-mysql -u root -p miniblog -e "show tables;"
-
+if ! mysql -u root -p miniblog -e "show tables;" | grep -q "miniblog"; then
+    echo "miniblog 数据库导入失败"
+    exit 1
+fi
