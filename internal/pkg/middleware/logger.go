304353c7dd4738f9992c9bba484f4980d78d2502
yangshujie <yangshujie@fangcunyisheng.com>
2025-05-20 13:32:07 +0800

    feat logger middleware, 同时记录请求 & 响应 日志

--- internal/pkg/middleware/logger.go
+++ internal/pkg/middleware/logger.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -11,11 +12,34 @@ import (
 	"github.com/yshujie/miniblog/internal/pkg/log"
 )
 
+// bodyLogWriter 是一个自定义的响应写入器，用于捕获响应体
+type bodyLogWriter struct {
+	gin.ResponseWriter
+	body *bytes.Buffer
+}
+
+func (w bodyLogWriter) Write(b []byte) (int, error) {
+	w.body.Write(b)
+	return w.ResponseWriter.Write(b)
+}
+
 // Logger 日志中间件
 func Logger() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		// 开始时间
 		startTime := time.Now()
+
+		// 读取请求体
+		var requestBody []byte
+		if c.Request.Body != nil {
+			requestBody, _ = io.ReadAll(c.Request.Body)
+			// 重新设置请求体，因为读取后需要重置
+			c.Request.Body = io.NopCloser(bytes.NewBuffer(requestBody))
+		}
+
+		// 创建自定义响应写入器
+		blw := &bodyLogWriter{body: bytes.NewBufferString(""), ResponseWriter: c.Writer}
+		c.Writer = blw
 
 		// 处理请求
 		c.Next()
@@ -47,20 +71,11 @@ func Logger() gin.HandlerFunc {
 		// 获取 User-Agent
 		userAgent := c.Request.UserAgent()
 
-		// 获取 POST 数据
-		var postData string
-		if c.Request.Method == "POST" {
-			body, err := io.ReadAll(c.Request.Body)
-			if err == nil {
-				postData = string(body)
-			}
-		}
-
 		// 获取 Cookies
 		cookies, _ := json.Marshal(c.Request.Cookies())
 
-		// 构建日志消息
-		msg := fmt.Sprintf("%s [%s][%d] [%.3f ms] [NTC] [-- [server_ip = %s] [client_ip = %s] [theUrl = %s] [referer = %s] [_USER_AGENT = %s] [posts = %s] [cookies = %s] --]",
+		// 构建请求日志消息
+		requestMsg := fmt.Sprintf("%s [%s][%d] [%.3f ms] [NTC] [-- [server_ip = %s] [client_ip = %s] [theUrl = %s] [referer = %s] [_USER_AGENT = %s] [request_body = %s] [cookies = %s] --]",
 			startTime.Format("15:04:05.000"),
 			requestID,
 			statusCode,
@@ -70,11 +85,23 @@ func Logger() gin.HandlerFunc {
 			reqUri,
 			referer,
 			userAgent,
-			postData,
+			string(requestBody),
 			string(cookies),
 		)
 
-		// 输出日志
-		log.C(c).Infow(msg)
+		// 构建响应日志消息
+		responseMsg := fmt.Sprintf("%s [%s][%d] [%.3f ms] [NTC] [-- [response_body = %s] --]",
+			endTime.Format("15:04:05.000"),
+			requestID,
+			statusCode,
+			float64(latencyTime.Microseconds())/1000,
+			blw.body.String(),
+		)
+
+		// 输出请求日志
+		log.C(c).Infow("HTTP Request", "request", requestMsg)
+
+		// 输出响应日志
+		log.C(c).Infow("HTTP Response", "response", responseMsg)
 	}
 }
