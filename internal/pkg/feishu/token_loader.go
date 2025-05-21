ed658a5aefd07b0ac119a3b3248e3e8722e1d0d6
yangshujie <yangshujie@fangcunyisheng.com>
2025-05-22 07:33:34 +0800

    test 测试 load token

--- internal/pkg/feishu/token_loader.go
+++ internal/pkg/feishu/token_loader.go
@@ -52,16 +52,19 @@ func (t *tokenLoader) LoadToken(ctx context.Context) (string, error) {
 		return "", fmt.Errorf("failed to get tenant access token: %s", larkcore.Prettify(resp.CodeError))
 	}
 
-	// 解析响应
-	var data struct {
-		TenantAccessToken string `json:"tenant_access_token"`
-		Expire            int    `json:"expire"`
-	}
-	if err := resp.JSONUnmarshalBody(&data, nil); err != nil {
-		log.Errorw("failed to parse tenant access token response", "error", err)
-		return "", fmt.Errorf("failed to parse tenant access token response: %v", err)
-	}
+	log.Infow("successfully got tenant access token", "resp: ", resp)
+	// // 解析响应
+	// var data struct {
+	// 	TenantAccessToken string `json:"tenant_access_token"`
+	// 	Expire            int    `json:"expire"`
+	// }
+	// if err := resp.JSONUnmarshalBody(&data, nil); err != nil {
+	// 	log.Errorw("failed to parse tenant access token response", "error", err)
+	// 	return "", fmt.Errorf("failed to parse tenant access token response: %v", err)
+	// }
 
-	log.Infow("successfully loaded tenant access token", "expire", data.Expire)
-	return data.TenantAccessToken, nil
+	// log.Infow("successfully loaded tenant access token", "expire", data.Expire)
+	// return data.TenantAccessToken, nil
+
+	return "123", nil
 }
