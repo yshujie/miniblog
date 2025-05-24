05aedb68489f3de7914d6ac7531c621901d3e31c
yangshujie <yangshujie@fangcunyisheng.com>
2025-05-24 13:35:38 +0800

    feat user model 新增字段

--- internal/miniblog/model/user.go
+++ internal/miniblog/model/user.go
@@ -8,14 +8,16 @@ import (
 )
 
 type UserM struct {
-	ID        int64     `gorm:"column:id;primary_key"`
-	Username  string    `gorm:"column:username;not null"`
-	Password  string    `gorm:"column:password;not null"`
-	Nickname  string    `gorm:"column:nickname"`
-	Email     string    `gorm:"column:email"`
-	Phone     string    `gorm:"column:phone"`
-	CreatedAt time.Time `gorm:"column:created_at"`
-	UpdatedAt time.Time `gorm:"column:updated_at"`
+	ID           int64     `gorm:"column:id;primary_key"`
+	Username     string    `gorm:"column:username;not null"`
+	Password     string    `gorm:"column:password;not null"`
+	Nickname     string    `gorm:"column:nickname"`
+	Avatar       string    `gorm:"column:avatar"`
+	Email        string    `gorm:"column:email"`
+	Phone        string    `gorm:"column:phone"`
+	Introduction string    `gorm:"column:introduction"`
+	CreatedAt    time.Time `gorm:"column:created_at"`
+	UpdatedAt    time.Time `gorm:"column:updated_at"`
 }
 
 // TableName 指定表名
